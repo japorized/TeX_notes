134a6ba4951ec43ee2b1db65931842cc185c907f
Japorized <johnson5756@live.com>
2018-08-17 17:24:03 -0400

    streamlining some stuff

---
+++ setup-nvim/tex_assignment.sh
@@ -0,0 +1,26 @@
+\documentclass[11pt, oneside]{book}
+
+\include{latex-assignment-preamble}
+
+% Main Body
+\title{title}
+\author{Johnson Ng}
+
+\begin{document}
+
+\begin{tabular*}{\textwidth}{l @{\extracolsep{\fill}} l @{\extracolsep{\fill}} l @{\extracolsep{\fill}} l}
+	Student name &: Ng Keng Seng	&	UW ID\#		&: 20635817 \\
+	Class 		 &: CLASSCODE		&	Assignment 	&: \# \\
+	Term 		 &: TERM 20XX
+\end{tabular*}
+
+\hrule
+
+\begin{enumerate}
+	\item Question
+	\begin{tcolorbox}[arc=0mm, breakable, enhanced]
+    Solution
+	\end{tcolorbox}
+\end{enumerate}
+
+\end{document}
